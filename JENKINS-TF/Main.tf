029b498895037ca5762456e2143b49db16f7037a
Aditi Nayak <109815646+aditiravindranayak@users.noreply.github.com>
2024-10-14 03:00:04 +0530

    Update Main.tf

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -1,55 +1,21 @@
 resource "aws_security_group" "Jenkins-sg" {
-  name        = "Jenkins-Security-Group"
-  description = "Open 22, 443, 80, 8080, 9000, and 3000"
+  name        = "Jenkins-Security Group"
+  description = "Open 22,443,80,8080,9000,3000"
 
-  # Define ingress rules to allow traffic on specified ports
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "SSH Access"
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "HTTP Access"
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24"]
-    description = "HTTPS Access"
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24"]
-    description = "Jenkins Access"
-  }
-
-  ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "SonarQube Access"
-  }
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Custom Access"
-  }
+  # Define a single ingress rule to allow traffic on all specified ports
+  ingress = [
+    for port in [22, 80, 443, 8080, 9000, 3000] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
 
   egress {
     from_port   = 0
@@ -63,18 +29,18 @@ resource "aws_security_group" "Jenkins-sg" {
   }
 }
 
+
 resource "aws_instance" "web" {
-  ami                    = "ami-09b0a86a2c84101e1" # Ensure this AMI is valid for your region
-  instance_type          = "t2.micro" # Changed to t2.micro to stay within Free Tier
-  key_name               = "react1" # Ensure you have created this key pair
+  ami                    = "ami-09b0a86a2c84101e1"
+  instance_type          = "t2.micro"
+  key_name               = "react1"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
 
   tags = {
     Name = "Jenkins-sonar"
   }
-
   root_block_device {
-    volume_size = 10 # Reduced size to stay within Free Tier limits
+    volume_size = 10
   }
 }
